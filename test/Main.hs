e7cf3451f88bf8cb79395f5c074171469331fa9c
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-12-15 14:18:58 +0100

    chore: add Game.Core tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,6 @@
 import Test.Closed.Extra qualified
 import Test.Game qualified
+import Test.Game.Core qualified
 import Test.Hspec (hspec)
 import Test.Hspec.Contrib.HUnit (fromHUnitTest)
 
@@ -7,3 +8,4 @@ main :: IO ()
 main = hspec $ do
   fromHUnitTest Test.Game.tests
   Test.Closed.Extra.tests
+  Test.Game.Core.tests
---
+++ test/Test/Game/Core.hs
@@ -0,0 +1,18 @@
+module Test.Game.Core (tests) where
+
+import Game.Core (Tile)
+import Test.Hspec (SpecWith, describe, it)
+import Test.Hspec.Expectations.Pretty (shouldBe)
+
+tests :: SpecWith ()
+tests = do
+  describe "Game.Core" $ do
+    describe "Tile" $ do
+      describe "Enum" $ do
+        it "assigns bounds to correct integers" $ do
+          fromEnum (minBound @Tile) `shouldBe` 0
+          -- 13 possible values * 4 possible colors = 52,
+          -- + 1 for the joker,
+          -- but we start from 0, so -1.
+          -- We end up with 52.
+          fromEnum (maxBound @Tile) `shouldBe` 52
