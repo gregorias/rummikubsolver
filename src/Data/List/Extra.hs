0906972a3f8e4a60fd4955a4a535d867ba18f0a3
Grzegorz Milka <grzegorzmilka@gmail.com>
2023-12-17 11:16:21 +0100

    feat: implement better partition

---
+++ src/Data/List/Extra.hs
@@ -0,0 +1,16 @@
+module Data.List.Extra (
+  partition,
+  PartitionSelect,
+) where
+
+import Relude (foldr)
+
+-- | A better partition function.
+partition :: (a -> PartitionSelect l r) -> [a] -> ([l], [r])
+partition f = foldr (select f) ([], [])
+ where
+  select f x ~(ls, rs) = case f x of
+    L y -> (y : ls, rs)
+    R y -> (ls, y : rs)
+
+data PartitionSelect l r = L l | R r
